acde7a35ec89f16fc36ad1e0d46a2b97bc4c03ee
Ivaylo1992 <ivailop123@gmail.com>
2025-01-05 10:16:21 +0200

    added to accounts funtionality

--- todoApp/accounts/urls.py
+++ todoApp/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from todoApp.accounts import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutAPIView.as_view(), name='logout')
+    path('logout/', views.LogoutAPIView.as_view(), name='logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token')
 ]
